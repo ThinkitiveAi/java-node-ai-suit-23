8b251004969455289b233c5b1afd12f9cf8779dc
Kedar Kapse <kedar.kapse@thinkitive.com>
2025-08-13 15:20:40 +0530

    Mark all files as changed after restoring original .git

---
+++ health-first-server/src/test/java/com/healthfirst/server/HealthFirstServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.healthfirst.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HealthFirstServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
